8979b8b370e1ef5a41fe06aa067ecda86d58dfbf
defx <mdonkin83@gmail.com>
2022-02-24 10:47:20 +0000

    conditional coverage

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { socketServer } from "./socketServer.js"
 import { getIstanbulCoverage } from "./getIstanbulCoverage.js"
 
 import { createReport } from "./createReport.js"
+import { loadPage } from "./loadPage.js"
 
 const TEST_SUITE_URL = "http://localhost:5001"
 
@@ -16,12 +17,14 @@ const argv = yargs(hideBin(process.argv)).argv
 async function start() {
   let p = socketServer()
   testSuiteServer()
-  const coverage = await getIstanbulCoverage(
-    `http://localhost:5001`,
-    `src/*.js`,
-    p
-  )
-  createReport(coverage)
+
+  if (argv.coverage) {
+    const coverage = await getIstanbulCoverage(TEST_SUITE_URL, `src/**/*.js`, p)
+    createReport(coverage)
+  } else {
+    loadPage(TEST_SUITE_URL)
+  }
+
   p.then((stats) => {
     process.exit(stats.failures === 0 ? 0 : 1)
   })
